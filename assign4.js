429e2462431d3ab57ed1c137dd11aaf00ed6f5c6
angie966 <angad6994@yahoo.in>
2017-01-21 22:25:21 +0000

    initial commit

--- assign4.js
+++ assign4.js
@@ -1,11 +1,18 @@
+// tip calculator program
 const myVal= process.argv;
+
 let Arra=[];
+
+// passing mealcost and tip-pecent from command line
 let myMealCost = Arra.push(Number(myVal[2]));
- let myTipPercent = Arra.push(Number(myVal[3])); 
+let myTipPercent = Arra.push(Number(myVal[3])); 
+
+
 const tipAmount = (Number(myVal[3]) / 100) * Number(myVal[2])
 
-
 const totalOwing = tipAmount + Number(myVal[2])
+
 const outPut = `your meal was $${Number(myVal[2])} + a ${Number(myVal[3])}% tip = ${totalOwing}`
 
+// console the information
 console.log(outPut)
