37f4e055f0aac3ff14e5683ab24d2254e6314151
angie966 <angad6994@yahoo.in>
2017-01-21 22:15:10 +0000

    initial commit

--- assign2.js
+++ assign2.js
@@ -1,7 +1,21 @@
+// using process.argv
 const myVal = process.argv;
+
+//creating array
 let Arr1=[];
-Arr1.push(Number(myVal[3]));
-Arr1.push(Number(myVal[4]));
+
+// third value 
+Arr1.push(myVal[2]);
+ 
+ // console log third value
 console.log(Arr1);
+
+// converting the 3rd and 4th value to number
+Arr1.push(Number(myVal[2]));
+Arr1.push(Number(myVal[3]));
+
+// adding both the values
 let a= Number(myVal[3])+ Number(myVal[4]);
+
+//console log the sum
 console.log(a);
