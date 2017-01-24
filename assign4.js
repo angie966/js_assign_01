56c12bd69b5a32bb9d1dfde21d68a3723030ccec
angie966 <angad6994@yahoo.in>
2017-01-24 23:13:18 +0000

    initial commit

--- assign4.js
+++ assign4.js
@@ -4,15 +4,15 @@ const myVal= process.argv;
 let Arra=[];
 
 // passing mealcost and tip-pecent from command line
-let myMealCost = Arra.push(Number(myVal[2]));
-let myTipPercent = Arra.push(Number(myVal[3])); 
+let myMealCost = Number(myVal[2]);
+let myTipPercent = Number(myVal[3]); 
 
 
-const tipAmount = (Number(myVal[3]) / 100) * Number(myVal[2])
+const tipAmount = (myTipPercent / 100) * myMealCost;
 
-const totalOwing = tipAmount + Number(myVal[2])
+const totalOwing = tipAmount + myMealCost;
 
-const outPut = `your meal was $${Number(myVal[2])} + a ${Number(myVal[3])}% tip = ${totalOwing}`
+const outPut = `your meal was $${myMealCost} + a ${myTipPercent}% tip = ${totalOwing}`
 
 // console the information
 console.log(outPut)
